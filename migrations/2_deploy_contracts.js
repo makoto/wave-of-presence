0a0c79654fddd4e23ae56d1a47a65b0d45f3f0a2
Makoto Inoue <inouemak@googlemail.com>
2017-12-13 14:56:18 +0700

    Add WebOfPresence to migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
+var WebOfPresence = artifacts.require("./WebOfPresence.sol");
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(WebOfPresence, 0x0);
 };
