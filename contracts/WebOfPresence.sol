000b5d459b97a91aeab14ccd1b4d0a5bc952f258
Makoto Inoue <inouemak@googlemail.com>
2017-12-14 09:24:03 +0700

    Refactor to use trust level as enum

--- contracts/WebOfPresence.sol
+++ contracts/WebOfPresence.sol
@@ -1,18 +1,20 @@
 pragma solidity ^0.4.18;
 
 contract WebOfPresence {
+  enum Trust { None, Full }
+
   struct Claim {
     bool present;
 		address _host;
-    int trust;
+    Trust trust;
 	}
   mapping (address => Claim) public claims;
 
   function WebOfPresence(address _host){
-    claims[_host] = Claim(true, 0x0, 3);
+    claims[_host] = Claim(true, 0x0, Trust.Full);
   }
 
-  function confirm(address _guest, int _trust) public {
+  function confirm(address _guest, Trust _trust) public {
     require(isPresent(msg.sender));
     claims[_guest] = Claim(true, msg.sender, _trust);
   }
@@ -21,3 +23,6 @@ contract WebOfPresence {
     return claims[_guest].present;
   }
 }
+
+/*0x14723a09acff6d2a60dcdf7aa4aff308fddc160c
+0xca35b7d915458ef540ade6068dfe2f44e8fa733c*/
--- test/web_of_presence.js
+++ test/web_of_presence.js
@@ -13,26 +13,26 @@ contract('WebOfPresence', function(accounts) {
     assert.equal((await presence.isPresent.call(guest)), false);
   });
 
-  describe.only('on confirm', function(){
+  describe('on confirm', function(){
     beforeEach(async function(){
       presence = await WebOfPresence.new(host);
     })
 
     it("host can confirm attendance", async function() {
-      await presence.confirm(volunteer, 3, {from:host});
+      await presence.confirm(volunteer, 1, {from:host});
       assert.equal((await presence.isPresent.call(volunteer)), true);
     });
 
     it("volunteer can not confirm unless confirmed first", async function() {
       assert.equal((await presence.isPresent.call(volunteer)), false);
-      await presence.confirm(guest, 3, {from:volunteer}).catch(function(){});
+      await presence.confirm(guest, 1, {from:volunteer}).catch(function(){});
       assert.equal((await presence.isPresent.call(guest)), false);
     });
 
     it("volunteer can confirm once confirmed", async function() {
-      await presence.confirm(volunteer, 3, {from:host});
+      await presence.confirm(volunteer, 1, {from:host});
       assert.equal((await presence.isPresent.call(volunteer)), true);
-      presence.confirm(guest, 3, {from:volunteer});
+      presence.confirm(guest, 1, {from:volunteer});
       assert.equal((await presence.isPresent.call(guest)), true);
     });
   })
