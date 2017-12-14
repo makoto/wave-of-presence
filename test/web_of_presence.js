0d4b3b051c6c3223f24536058743d63f4e70fa8f
Makoto Inoue <inouemak@googlemail.com>
2017-12-14 09:30:36 +0700

    Refactor test to use Trust.Full

--- test/web_of_presence.js
+++ test/web_of_presence.js
@@ -1,4 +1,5 @@
 const WebOfPresence = artifacts.require("./WebOfPresence.sol");
+const Trust = {None:0, Full:1};
 
 contract('WebOfPresence', function(accounts) {
   let presence;
@@ -22,21 +23,21 @@ contract('WebOfPresence', function(accounts) {
     })
 
     it("host can confirm attendance", async function() {
-      await presence.confirm(volunteer, 1, {from:host});
+      await presence.confirm(volunteer, Trust.Full, {from:host});
       assert.equal((await presence.isPresent.call(volunteer)), true);
     });
 
     it("volunteer can not confirm unless confirmed first", async function() {
       assert.equal((await presence.isPresent.call(volunteer)), false);
-      await presence.confirm(guest, 1, {from:volunteer}).catch(function(){});
+      await presence.confirm(guest, Trust.Full, {from:volunteer}).catch(function(){});
       assert.equal((await presence.isPresent.call(guest)), false);
     });
 
     it("volunteer can confirm once confirmed", async function() {
-      await presence.confirm(volunteer, 1, {from:host});
+      await presence.confirm(volunteer, Trust.Full, {from:host});
       assert.equal((await presence.isPresent.call(volunteer)), true);
       assert.equal((await presence.isTrusted.call(volunteer)), true);
-      presence.confirm(guest, 1, {from:volunteer});
+      presence.confirm(guest, Trust.Full, {from:volunteer});
       assert.equal((await presence.isPresent.call(guest)), true);
     });
   })
