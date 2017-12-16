924f4d989d5b159affb06cee95dd2ebd68812998
Makoto Inoue <inouemak@googlemail.com>
2017-12-16 22:32:33 +0700

    Send transaction to confirm function

--- src/HostPanel.js
+++ src/HostPanel.js
@@ -3,14 +3,20 @@ import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import Avatar from 'material-ui/Avatar';
 import { web3, uport } from './utils/uportSetup.js'
+import { MNID } from 'uport-connect'
+import { MyContractSetup } from './contractSetup.js'
+import { waitForMined, pollingLoop } from './utils/contractHandeling.js'
+// import { getCurrentDataFromChain } from './utils/utilities.js'
 
+const MyContract = MyContractSetup()
+console.log('window.MyContract', window.MyContract)
+window.MyContract = MyContract;
 const HostPanel = (props) => {
   let result;
   switch (props.stage) {
     case 'login':
       result = (
         <div>
-          <h2>Host: </h2>
           <RaisedButton
            label="Log in as a host" primary={true}
            onClick={()=>{
@@ -19,6 +25,7 @@ const HostPanel = (props) => {
                notifications: true // We want this if we want to recieve credentials
              })
              .then((credentials) => {
+               console.log('credentials', credentials);
                props.event.emit('stage', {
                  stage: 'checkin',
                  host:credentials.name,
@@ -51,7 +58,28 @@ const HostPanel = (props) => {
           </div>
           <RaisedButton label="Confirm guest" primary={true}
             onClick={()=>{
-              props.event.emit('stage', {stage:'proof'})
+              // let host_address = "0x5a384227b65fa093dec03ec34e111db80a040615"
+              let host_address = "2odgxKGQvWfrhaAhJ53dc66tssZkh4j8x9b"
+              const decodedId = MNID.decode(host_address).address;
+              console.log('decodedId', decodedId);
+              let gest_address = ""
+              // Transaction signing (that will fire a QR to scan or card in the mobile app)
+              MyContract.confirm(decodedId, 1, (error, txHash) => {
+                if (error) { throw error }
+                  waitForMined(txHash, { blockNumber: null }, // see next area
+                  function pendingCB () {
+                    // Signal to the user you're still waiting
+                    // for a block confirmation
+                    console.log("pendingCB")
+                  },
+                  function successCB (data) {
+                    // Great Success!
+                    // Likely you'll call some eventPublisherMethod(txHash, data)
+                    console.log("success", data);
+                    props.event.emit('stage', {stage:'proof'});
+                  }
+                )
+              })
             }}
           />
         </div>
